dafeb14c945066b0e416fc231cea20c8b3f5aa67
phush <phush@ikmail.com>
2026-05-30 18:00:13 +1200

    zellij: pipe into programs.zellij.themes

--- home-modules/zellij.nix
+++ home-modules/zellij.nix
@@ -13,4 +13,18 @@ in
     enable = lib.options.mkEnableOption "Enable Rose Pine theme for Zellij";
     package = self.packages.${pkgs.stdenv.hostPlatform.system}.zellij;
   };
+
+  config = lib.mkIf cfg.enable {
+    programs.zellij.themes =
+      let
+        themesDir = "${cfg.package}/share/zellij/themes";
+        entries = builtins.readDir themesDir;
+        themeFiles = lib.filterAttrs (_: type: type == "regular") entries;
+      in
+      lib.mapAttrs' (name: _: {
+        name = lib.removeSuffix ".kdl" name;
+        value = builtins.readFile "${themesDir}/${name}";
+      }) themeFiles;
+
+  };
 }
